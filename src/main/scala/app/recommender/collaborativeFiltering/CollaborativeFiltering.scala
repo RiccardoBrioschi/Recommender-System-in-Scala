b594af400f9ff17ddcd60c407e8f6e50a722f3f2
RiccardoBrioschi <brioschiriccardo.job@gmail.com>
2023-04-08 11:55:38 +0200

    als using mllib

--- src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
+++ src/main/scala/app/recommender/collaborativeFiltering/CollaborativeFiltering.scala
@@ -3,6 +3,10 @@ package app.recommender.collaborativeFiltering
 
 import org.apache.spark.rdd.RDD
 
+import org.apache.spark.mllib.recommendation.ALS
+import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
+
 class CollaborativeFiltering(rank: Int,
                              regularizationParameter: Double,
                              seed: Long,
@@ -10,10 +14,22 @@ class CollaborativeFiltering(rank: Int,
 
   // NOTE: set the parameters according to the project description to get reproducible (deterministic) results.
   private val maxIterations = 20
-  private var model = null
+  private var model: MatrixFactorizationModel = null
 
-  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = ???
+  def init(ratingsRDD: RDD[(Int, Int, Option[Double], Double, Int)]): Unit = {
 
-  def predict(userId: Int, movieId: Int): Double = ???
+    val temp_model = new ALS().setSeed(seed).setRank(rank).setIterations(maxIterations)
+      .setLambda(regularizationParameter).setBlocks(n_parallel)
+
+    val correct_rating = ratingsRDD.map(term => (term._1, term._2, term._4))
+      .map(term => new Rating(term._1, term._2, term._3))
+
+    model = temp_model.run(correct_rating)
+  }
+
+
+  def predict(userId: Int, movieId: Int): Double =
+
+    model.predict(userId, movieId)
 
 }
